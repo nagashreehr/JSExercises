62170806828c80139f57d375cec4389ba67e3ac8
Nagashreehr <nagashreehr2016@gmail.com>
2022-11-27 19:56:05 +0530

    second commit

--- Ex9.js
+++ Ex9.js
@@ -1,16 +1,26 @@
-const nearestLargestNumber = (arr, index) => {
-    const num = arr[index];
-    let nearestLargest;
-    const findIdxOfNearestLargest = (idxOfNearestLargest, item, idx2) => {
-        if (item > num &&
-            (idxOfNearestLargest === null || nearestLargest > item)) {
-            nearestLargest = item;
-            idxOfNearestLargest = idx2;
+function nearnumber(array, value) {
+    let result;
+    let x = 0;
+    let dist = [];
+    for (var i = 0; i < array.length; i++) {
+        if (value == array[i]) {
+            x = i;
         }
-        return idxOfNearestLargest;
-    };
+    }
 
-    return arr.reduce(findIdxOfNearestLargest, null);
-};
+    for (var i = 0; i < array.length; i++) {
+        if (array[i] > value) {
+            var temp1 = Math.abs(x - i);
+            dist.push(temp1);
+        }
+    }
+    let temp;
+    for (var i = 0; i < dist.length - 1; i++) {
+        temp = Math.min(...dist);
+    }
+    res = array[temp + x];
+    console.log(res);
+}
 
-console.log(nearestLargestNumber([1, 4, 3, 2, 5, 7], 7));
+var a = [1, 4, 3, 2, 5, 7];
+nearnumber(a, 2);
